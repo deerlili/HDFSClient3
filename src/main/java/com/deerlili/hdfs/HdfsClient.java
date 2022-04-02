574f8e27b84091fa0048cde7b65243a92c0119d9
李绪兴 <lixx@cqbanshan.com>
2022-04-02 12:39:19 +0800

    judge catalog or file

--- src/main/java/com/deerlili/hdfs/HdfsClient.java
+++ src/main/java/com/deerlili/hdfs/HdfsClient.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,6 +20,8 @@ import java.util.Arrays;
  */
 public class HdfsClient {
 
+    private static final Logger logger = LoggerFactory.getLogger(HdfsClient.class);
+
     private FileSystem hdfs;
 
     @Before
@@ -26,7 +30,7 @@ public class HdfsClient {
         Configuration configuration = new Configuration();
         //configuration.set("dfs.replication","2");
         String user = "root";
-        hdfs = FileSystem.get(uri, configuration,user);
+        hdfs = FileSystem.get(uri, configuration, user);
     }
 
     @After
@@ -36,7 +40,8 @@ public class HdfsClient {
 
     @Test
     public void testMkdir() throws IOException {
-        hdfs.mkdirs(new Path("/test"));
+        Path path = new Path("/test");
+        hdfs.mkdirs(path);
     }
 
     /**
@@ -45,33 +50,34 @@ public class HdfsClient {
      * （2）ClassPath下的用户自定义配置文件
      * （3）然后是服务器的自定义配置(xxx-site.xml)
      * （4）服务器的默认配置(xxx-default.xml)
+     *
      * @throws IOException
      */
     @Test
     public void testPut() throws IOException {
         Path src = new Path("E:\\11-软考高级\\deerlili.txt");
         Path dst = new Path("/test");
-        hdfs.copyFromLocalFile(false,true,src,dst);
+        hdfs.copyFromLocalFile(false, true, src, dst);
     }
 
     @Test
     public void testGet() throws IOException {
         Path src = new Path("/test");
         Path dst = new Path("E:\\");
-        hdfs.copyToLocalFile(false,src,dst,true);
+        hdfs.copyToLocalFile(false, src, dst, true);
     }
 
     @Test
     public void testRm() throws IOException {
         Path path = new Path("/output");
-        hdfs.delete(path,true);
+        hdfs.delete(path, true);
     }
 
     @Test
     public void testMv() throws IOException {
         Path src = new Path("/input");
         Path dst = new Path("/output");
-        hdfs.rename(src,dst);
+        hdfs.rename(src, dst);
     }
 
     @Test
@@ -81,22 +87,30 @@ public class HdfsClient {
 
         while (listFiles.hasNext()) {
             LocatedFileStatus fileStatus = listFiles.next();
-            System.out.println(fileStatus.getPermission());
-            System.out.println(fileStatus.getOwner());
-            System.out.println(fileStatus.getGroup());
-            System.out.println(fileStatus.getLen());
-            System.out.println(fileStatus.getModificationTime());
-            System.out.println(fileStatus.getReplication());
-            System.out.println(fileStatus.getBlockSize());
-            System.out.println(fileStatus.getPath().getName());
+
+            logger.info("file info: {}", fileStatus.getPermission());
+            logger.info("file info: {}", fileStatus.getOwner());
+            logger.info("file info: {}", fileStatus.getGroup());
+            logger.info("file info: {}", fileStatus.getLen());
+            logger.info("file info: {}", fileStatus.getModificationTime());
+            logger.info("file info: {}", fileStatus.getReplication());
+            logger.info("file info: {}", fileStatus.getBlockSize());
+            logger.info("file info: {}", fileStatus.getPath().getName());
 
             BlockLocation[] blockLocations = fileStatus.getBlockLocations();
-
-            System.out.println(Arrays.toString(blockLocations));
+            logger.info("file info: {}", Arrays.toString(blockLocations));
         }
-
     }
 
-
-
+    @Test
+    public void testFile() throws IOException {
+        FileStatus[] listStatus = hdfs.listStatus(new Path("/"));
+        for (FileStatus status : listStatus) {
+            if (status.isFile()) {
+                logger.info("文件: {}", status.getPath().getName());
+            } else {
+                logger.info("目录: {}", status.getPath().getName());
+            }
+        }
+    }
 }
